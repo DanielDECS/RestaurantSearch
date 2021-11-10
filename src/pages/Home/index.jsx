101f7535f949313d56295a2960919b772b365cb4
Daniel <dancarsoa@gmail.com>
2021-11-09 23:18:59 -0300

    Criada a estrutura do projeto

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Container } from './styles';
+
+const Home = () => <Container>Olá Pessoal</Container>;
+
+export default Home;
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+        primary: '#003fff',
+        background: '#000000',
+        text: '#000000B3',
+        success: '#135E01',
+    },
+    fonts: {
+        fontFamily: 'Roboto, sans-serif',
+    },
+};
+
+export default theme;
